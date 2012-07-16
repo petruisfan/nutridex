be3e3c04c0a22008233ea36275ba3120c0432cd6
petru.isfan <petru.isfan@gmail.com>
2012-07-16 15:42:35 +0000

    Added table reprezentation for data.

--- src/com/mysite/nutridex/FoodActivity.java
+++ src/com/mysite/nutridex/FoodActivity.java
@@ -1,28 +1,31 @@
 package com.mysite.nutridex;
 
-import com.mysite.nutridex.db.AbstractTable;
 import com.mysite.nutridex.db.DatabaseAdapter;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class FoodActivity extends ListActivity {
+public class FoodActivity extends Activity {
 
 	private DatabaseAdapter dbAdapter;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.food_list);
+//        setContentView(R.layout.food_list);
+        setContentView(R.layout.food_table);
+        
         dbAdapter = new DatabaseAdapter(this);
         dbAdapter.open();
+        
         fillData();
     }
 
@@ -30,24 +33,51 @@ public class FoodActivity extends ListActivity {
     	Bundle extras = getIntent().getExtras();
     	int tableNr = extras.getInt(getString(R.string.table));
     	Cursor notesCursor = dbAdapter.fetchTable(tableNr);
-
     	
     	fillHeaders();
-    	
-        // Get all of the rows from the database and create the item list
-        startManagingCursor(notesCursor);
-
-        // Create an array to specify the fields we want to display in the list (only TITLE)
-        String[] from = AbstractTable.getTableColumns();
-        
-        // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5,};
-
-        // Now create a simple cursor adapter and set it to display
-        SimpleCursorAdapter notes = new SimpleCursorAdapter(this, R.layout.food_row, notesCursor, from, to);
-        
-        setListAdapter(notes);
+    	fillTable(notesCursor);
+//    	fillList(notesCursor);
     }
+
+	private void fillTable(Cursor cursor) {
+		cursor.moveToFirst();
+
+		TableLayout tl = (TableLayout) findViewById(R.id.food_table);
+		
+		TableRow row;
+		while (cursor.isAfterLast() == false) {
+			row = new TableRow(getApplicationContext());
+		    
+			int cols = cursor.getColumnCount();
+			
+			for (int i=1; i<cols; i++ ) {
+				String value = cursor.getString(i);
+				TextView cell = new TextView(getApplicationContext());
+				cell.setText(value);
+				
+				row.addView(cell);
+			}
+			tl.addView(row);
+			
+		    cursor.moveToNext();
+		}
+	}
+
+//	private void fillList(Cursor notesCursor) {
+//		// Get all of the rows from the database and create the item list
+//        startManagingCursor(notesCursor);
+//
+//        // Create an array to specify the fields we want to display in the list (only TITLE)
+//        String[] from = AbstractTable.getTableColumns();
+//        
+//        // and an array of the fields we want to bind those fields to (in this case just text1)
+//        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5,};
+//
+//        // Now create a simple cursor adapter and set it to display
+//        SimpleCursorAdapter notes = new SimpleCursorAdapter(this, R.layout.food_row, notesCursor, from, to);
+//        
+//        setListAdapter(notes);
+//	}
 
 	private void fillHeaders() {
 		String[] headersText = getResources().getStringArray(R.array.columns);
